522cc7e1c721fbb2f5c71577684f98f62317279d
jill bhatt <bhattjill@gmail.com>
2022-05-31 18:29:12 +0530

    Changed pixabay key identifier

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 const fetchImages = async (searchTerm: string) => {
     const url: string = `https://pixabay.com/api/?key=${
-        process.env.REACT_APP_PIXABAY_API
+        process.env.REACT_APP_PIXABAY_API_KEY
     }&q=${searchTerm.trim()}&image_type=photo&pretty=true`;
 
     const imagesResponse: AxiosResponse = await axios({
